4b8491190b7b53896af488c0e2df4469c4e198c3
Shridhar Patil <Shridhar.g.patil1@gmail.com>
2026-05-18 01:23:54 +0530

    updated readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -22,5 +22,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-html_logo = "_static/logo.png"
+html_logo = "_static/icon.png"
 html_favicon = "_static/fav.ico"
